43bfd7834ef0e046bd91325a4783f01451abc8c5
James <jamesscofield2020@gmail.com>
2020-07-03 00:34:26 +0300

    er

--- roman.py
+++ roman.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, requestapp = Flask(__name__)def convert(num):
+from flask import Flask, render_template, requestapp = Flask(__name__)
+def convert(num):
     num = int(num)    mappings = {1000: "M", 900: 'CM', 500: "D", 400: 'CD', 100: "C",
                 90: 'XC', 50: "L", 40: 'XL', 10: "X", 9: 'IX', 5: "V", 4: 'IV', 1: "I"}    result = ""    for k,v in mappings.items() : # k =1000
         value = num // k # 1
