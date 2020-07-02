34d6a05cdc0667f4229c234b084e935631e077fb
James <jamesscofield2020@gmail.com>
2020-07-03 00:57:06 +0300

    aga

--- roman.py
+++ roman.py
@@ -8,15 +8,18 @@ def convert(num):
     for k,v in mappings.items() : # k =1000
         value = num // k # 1
         result += v * value # M *1
-        num%=k # 994    return result@app.route("/", methods=["GET"])
+        num%=k # 994    
+    return result
+@app.route("/", methods=["GET"])
 def index():
-    return render_template("index.html", not_valid=False, developer_name="codebenders")@app.route("/", methods=["POST"])
+    return render_template("index.html", not_valid=False, developer_name="codebenders")
+@app.route("/", methods=["POST"])
 def result():
     decimal = request.form.get("number")
     if not decimal.isdecimal() or (not 0< int(decimal)<4000):
         return render_template("index.html", not_valid = True, developer_name = "codebenders")
     else:
         return render_template("result.html", number_decimal= decimal, developer_name= "codebenders", number_roman= convert(decimal))
-        if __name__ == "__main__":
+if __name__ == "__main__":
     #app.run("localhost", port=5000, debug=True)
-            app.run(debug=True)
+    app.run(debug=True)
