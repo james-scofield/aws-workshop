fc4af683c9285ca35e9a7d71dd3022a77626b8f3
James <jamesscofield2020@gmail.com>
2020-07-03 16:23:17 +0300

    henryversion

--- 001-roman-numerals-converter/app.py
+++ 001-roman-numerals-converter/app.py
@@ -1,24 +1,26 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
-@app.route("/", methods=["GET", "POST"])
+def convert(num):
+    num = int(num)
+    mappings = {1000: "M", 900: 'CM', 500: "D", 400: 'CD', 100: "C",
+                90: 'XC', 50: "L", 40: 'XL', 10: "X", 9: 'IX', 5: "V", 4: 'IV', 1: "I"}
+    result = "" 
+    for k,v in mappings.items() : # k =1000
+        value = num // k # 1
+        result += v * value # M *1
+        num%=k # 994    
+    return result
+@app.route("/", methods=["GET"])
 def index():
-    if request.method == "POST":
-        num = request.form.get("number")
-        if not num.isdecimal() or (not 0 < int(num) < 4000):
-            return render_template("index.html", not_valid=True, developer_name="codebenders")
-        else:
-            num = int(num)
-            num1 = num
-            mappings = {1000: "M", 900: 'CM', 500: "D", 400: 'CD', 100: "C",
-                        90: 'XC', 50: "L", 40: 'XL', 10: "X", 9: 'IX', 5: "V", 4: 'IV', 1: "I"}
-            res = ""
-            for k, v in mappings.items():  # k =1000
-                value = num // k  # 1
-                res += v * value  # M *1
-                num %= k  # 994        return render_template("result.html", number_decimal=num1, developer_name="codebenders", number_roman=res)
+    return render_template("index.html", not_valid=False, developer_name="codebenders")
+@app.route("/", methods=["POST"])
+def result():
+    decimal = request.form.get("number")
+    if not decimal.isdecimal() or (not 0< int(decimal)<4000):
+        return render_template("index.html", not_valid = True, developer_name = "codebenders")
     else:
-        return render_template("index.html", not_valid=False, developer_name="codebenders")
+        return render_template("result.html", number_decimal= decimal, developer_name= "codebenders", number_roman= convert(decimal))
 if __name__ == "__main__":
     #app.run("localhost", port=5000, debug=True)
-    #app.run(debug=True, port=8000)
+    #app.run(debug=True)
     app.run('0.0.0.0', port=80)
